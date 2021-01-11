f877a4b2f1ff34085fff13b744e12bc081c194cd
adnapp <adam.napps@gmail.com>
2021-01-11 16:25:25 -0500

    commit tree c0ee273157140d3024c997a474bbfb4ee4528c0d

---
+++ src/components/ArticleList.js
@@ -0,0 +1,25 @@
+import Article from './Article'
+
+function ArticleList({posts}){
+    // console.log(posts)
+    const articleItems = posts.map((post) => (
+        <Article
+           key= {post.id} 
+           title={post.title}
+           date={post.date}
+           preview={post.preview}
+           minutes={post.minutes}
+        />
+    ))
+
+    return( 
+        <main>
+            {articleItems}
+        </main>
+
+    )
+
+}
+
+
+export default ArticleList;
